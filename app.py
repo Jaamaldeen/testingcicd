53c4511c1dad756dbc8875b459183d61443bd944
Oyetunji Jamaldeen <oyetunjijayman@gmail.com>
2026-09-24 10:14:42 +0100

    update backend

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Ecommerce backend is running!"
+    return "Ecommerce backend is running good!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
